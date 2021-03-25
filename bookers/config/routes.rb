77d7fc538a2ffc5f3358fc61e751d101ec2e6820
mukku69 <programing0803@gmail.com>
2021-03-25 14:25:54 +0000

    [Start]3-8

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
+  get "/" => "homes#top"
+  get "books" => "books#index"
+  get "books/:id" => "books#show"
 end
