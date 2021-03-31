24ff85daaf871bf476124682eeaa201a94a24c84
mukku69 <programing0803@gmail.com>
2021-03-31 03:49:59 +0000

    [Finish]3-8

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
     @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -9,9 +9,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render("books/index")
+    end
   end
 
   def edit
@@ -19,15 +24,21 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render("books/edit")
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
   private
--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
-  get "/" => "homes#top"
+  root :to  => "homes#top"
   post "books" => "books#create"
   get "books" => "books#index"
   get "books/:id" => "books#show",as:"book"
